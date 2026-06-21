7f6d89ef46a5276fdc7f9d44e8a9922b605c5b76
Gyasuddin <gyasuddinansari7854@gmail.com>
2026-06-21 12:53:04 +0530

    server file changes

--- server/index.js
+++ server/index.js
@@ -22,8 +22,12 @@ app.use('/api/boards', require('./routes/boards'));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  app.use((req, res, next) => {
+    if (req.method === 'GET' && !req.path.startsWith('/api')) {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    } else {
+      next();
+    }
   });
 }
 
